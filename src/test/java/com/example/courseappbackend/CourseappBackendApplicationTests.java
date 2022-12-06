20939195200476708ae7703a8dad5f1ee156d21e
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-06 10:53:19 +0530

    first commit

---
+++ src/test/java/com/example/courseappbackend/CourseappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
